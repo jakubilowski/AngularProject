1550276f114834e24ed7e258110f226e56e9ba67
jakubilowski <jakub.ilowski@issrfid.com>
2023-05-31 12:10:45 +0200

    dodano formularz kontaktowy

--- MyAngularProject/src/app/MyComponents/main/contact/contact.component.ts
+++ MyAngularProject/src/app/MyComponents/main/contact/contact.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
 export class ContactComponent {
+  strEmail = '';
+  strMessage = '';
 
+  btnSendClick() {
+    alert(
+      'Udało się skorzystać z formularza, Twoje wpisane dane:' +
+        '\n Email: ' +
+        this.strEmail +
+        '\n Wiadomosć: ' +
+        this.strMessage
+    );
+  }
 }
